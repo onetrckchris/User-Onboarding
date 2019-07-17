545e1d3dda51de0861af45f5f9e4d07e58aaa700
onetrckchris <onetrckchris@gmail.com>
2019-07-17 12:21:40 -0700

    created Form component and am rendering in App

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import Form from './components/Form';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Form />
     </div>
   );
 }
